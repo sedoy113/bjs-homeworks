66cf18238672d3e4356fcda9435980b3d04bc69b
sedoy113 <sedoy113@yandex.ru>
2021-03-03 21:59:23 +0300

    Вторая задача

--- 2.1-functions/task.js
+++ 2.1-functions/task.js
@@ -1,5 +1,6 @@
 'use srict'
 
+// Задача №1. Корни квадратного уравнения
 function getSolutions(a, b, c) {
 	let D = b ** 2 - 4 * a * c;
 
@@ -27,5 +28,43 @@ function showSolutionsMessage(a, b, c) {
 		console.log(`Уравнение имеет два корня. X₁ = ${result.roots[0]}, X₂ = ${result.roots[1]}`);
 	}
 };
-
+showSolutionsMessage(1, 2, 3);
+showSolutionsMessage(7, 20, -3);
 showSolutionsMessage(2, 4, 2);
+
+// Задача №2. Журнал успеваемости
+
+function getAverageScore(data) {
+	let average = [];
+
+	for (let prop in data) {
+		data[prop] = getAverageMark(data[prop]);
+		average.push(data[prop])
+	}
+	data['average'] = getAverageMark(average);
+	// console.log(data['average']);
+	console.log(data);
+	return data;
+}
+
+function getAverageMark(marks) {
+	let reduceResult = (accumulate, el) => accumulate + el;
+	if (marks.length === 0) {
+		return 0;
+	} else {
+		return marks.reduce(reduceResult) / marks.length;
+	}
+}
+
+getAverageScore({
+	algebra: [2, 4, 5, 2, 3, 4],
+	geometry: [2, 4, 5],
+	russian: [3, 3, 4, 5],
+	physics: [5, 5],
+	misic: [2, 2, 6],
+	english: [4, 4, 3],
+	poetry: [5, 3, 4],
+	chemistry: [2],
+	french: [4, 4]
+});
+

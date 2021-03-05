3361d71f70db3430cf15765a7c4d5153ae0f94ff
sedoy113 <sedoy113@yandex.ru>
2021-03-05 19:50:18 +0300

    Выполнил первое и второе

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,38 @@
+'use strict'
 //String.prototype.isPalindrome - для задачи №1
 
+String.prototype.isPalindrome = function isPalindrome() {
+
+	const cleanString = this.toLowerCase().replace(/\s+/g, '');
+	const cleanStringRevers = cleanString.split("").reverse().join("");
+
+	return cleanString === cleanStringRevers;
+}
+
+// Задача № 2
+
+function getAverageScore(data) {
+	let average = [];
+
+	for (let prop in data) {
+		data[prop] = getAverageMark(data[prop]);
+		average.push(data[prop])
+	}
+	data['average'] = getAverageMark(average);
+	return data;
+}
+
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+	let roundedAverage;
+	let reduceResult = (accumulate, el) => accumulate + el;
+	if (marks.length === 0) {
+		return 0;
+	} else {
+		return roundedAverage = Math.round(marks.reduce(reduceResult) / marks.length);
+	}
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+	// код для задачи №3 писать здесь
+	// return verdict
 }

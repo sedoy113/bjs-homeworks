ae700560bccc7df3da30ccb27c74d7b8783a3afc
sedoy113 <sedoy113@yandex.ru>
2021-03-20 20:01:41 +0300

    lesson 2

--- 3.1-exceptions-closure/task.js
+++ 3.1-exceptions-closure/task.js
@@ -1,3 +1,5 @@
+'use strict'
+
 function parseCount(number) {
 	let resultNumber = Number.parseInt(number);
 	if (Number.isNaN(resultNumber)) {
@@ -15,5 +17,42 @@ function validateCount(parse) {
 	}
 }
 
-console.log(parseCount('12i'));
+// console.log(parseCount('12i'));
+
 //Задание 2
+class Triangle {
+	constructor(a, b, c) {
+		if ((a + b) < c || (b + c) < a || (a + c) < b) {
+			throw new Error('Треугольник с такими сторонами не существует');
+		}
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+
+	getPerimeter() {
+		return Number.parseInt(this.a + this.b + this.c);
+	}
+
+	getArea() {
+		let semiPerimeter = this.getPerimeter() / 2;
+		let areaTriangle = Math.sqrt(semiPerimeter * (semiPerimeter - this.a) * (semiPerimeter - this.b) * (semiPerimeter - this.c));
+		let newAreaTriangle = parseFloat(areaTriangle.toFixed(3));
+		return newAreaTriangle;
+	}
+}
+
+function getTriangle(a, b, c) {
+	try {
+		return new Triangle(a, b, c);
+	} catch (e) {
+		return {
+			getArea() {
+				return "Ошибка! Треугольник не существует";
+			},
+			getPerimeter() {
+				return "Ошибка! Треугольник не существует";
+			}
+		}
+	}
+}

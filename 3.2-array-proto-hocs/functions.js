18b34c4c3f39932fad1cccb61c3b5708a8c0bff4
sedoy113 <sedoy113@yandex.ru>
2021-03-21 15:35:57 +0300

    task_3.2

--- 3.2-array-proto-hocs/functions.js
+++ 3.2-array-proto-hocs/functions.js
@@ -1,4 +1,5 @@
-console.clear();
+'use strict';
+// task_1
 
 const weapons = [new Knife(), new Staff(), new Axe(), new StormStaff(), new LongBow(), new Bow()];
 // console.log(weapons);
@@ -27,4 +28,65 @@ function getReliableWeaponsNames(durability) {
 function getTotalDamage() {
 	return (weapons.map(weapon => weapon.attack)).reduce((accumulate, el) => accumulate + el);
 }
-console.log(getTotalDamage());
+// console.log(getTotalDamage());
+
+// task_2
+
+// 2.1 Подготовка к заданию
+
+// 2.1.1. sleep
+function sleep(milliseconds) {
+	let e = new Date().getTime() + milliseconds;
+	while (new Date().getTime() <= e) { }
+}
+
+// 2.1.2. sum
+function sum(...args) {
+	// Замедление на половину секунды.
+	sleep(100); // Можно использовать другое значение замедления.
+	return args.reduce((sum, arg) => {
+		return sum += +arg;
+	}, 0);
+}
+
+// 2.1.3. compareArrays
+function compareArrays(arr1, arr2) {
+	return arr1.length === arr2.length ? arr1.every((value, index) => value === arr2[index]) : false;
+}
+
+console.log(compareArrays([8, 9], [6])); // false, разные значения
+console.log(compareArrays([8, 9, 5, 4], [8, 9, 5, 4, 8, 3, 5])); // false, разные значения
+console.log(compareArrays([9, 2, 4, 8, 2], [9, 2, 4])); // false, разные значения
+console.log(compareArrays([1, 2, 3], [2, 3, 1])); // false, разные индексы, хотя и одинаковые значения
+console.log(compareArrays([8, 1, 2], [8, 1, 2])); // true
+
+// 2.2 Обёртка над оптимизацией любой функции
+
+function memorize(fn, limit) {
+	const memory = []
+	return function (...args) {
+		// 2.3.1. Поиск
+		let isMemory = memory.find(function (arr) {
+			return compareArrays(arr.args, args);
+		})
+		if (isMemory) {
+			return isMemory.result;
+		}
+		if (memory.length === limit) {
+			memory.shift();
+		}
+		let result = fn(...args)
+		memory.push({ args: args, result: result });
+		// console.log(memory);
+		return result;
+	}
+}
+
+// 2.2.1. Вызов результата
+const mSum = memorize(sum, 2); // 5 результатов может хранится в памяти
+// const mSum = memorize(sum, 2);
+console.log(sum(3, 4));
+console.log(mSum(3, 4));// 7
+console.log(mSum(1, 3));// 4
+
+
